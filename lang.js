4b33f01beebdc87948906133718a5c9ee98931f2
Bellisario <72039923+Bellisario@users.noreply.github.com>
2020-11-21 11:22:38 +0100

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,1 +1,32 @@
-navigator.lang = navigator.language.substr(0, 2).toLowerCase()
+/*
+lang.js by @Bellisario, GitHub
+https://github.com/Bellisario/lang.js
+         ____   
+        |  _ \  
+        | |_| | 
+        |  _ <  
+        | |_| | 
+        |____/  
+           _
+       .__(.)<
+        \___)   
+
+*/
+(function () {
+    navigator.lang = navigator.language.substr(0, 2).toLowerCase();
+    navigator.islang = function (_lang) {
+        return navigator.lang == _lang;
+    };
+    navigator.iflang = function (_lang, callback) {
+        if (navigator.lang == _lang.toString()) {
+            if (typeof callback != "function") {
+                return console.error("navigator.iflang needs a callback function to work correctly.");
+            };
+            return callback();
+        };
+    };
+    lang_js_version = function () {
+        return "0.1";
+    };
+    return true;
+})();
